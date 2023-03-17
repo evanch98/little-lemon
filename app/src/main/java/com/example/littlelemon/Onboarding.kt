d1c485fa97ba41d65251611ef2c7da63b4c81c88
Kyaw Thu <evanch98@gmail.com>
2023-03-17 14:29:44 +0800

    Initial implementation of the OnBoarding UI

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -1,16 +1,31 @@
 package com.example.littlelemon
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun OnBoarding() {
-    Image(
-        painter = painterResource(id = R.drawable.little_lemon_logo),
-        contentDescription = "Logo Image"
-    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.little_lemon_logo),
+            contentDescription = "Logo Image",
+            contentScale = ContentScale.Fit
+        )
+        Text(
+            text = "Let's get to know you",
+        )
+    }
 }
 
 @Preview(showBackground = true)
