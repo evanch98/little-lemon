37edee7713f1d08796f95b65437200248253680b
Kyaw Thu <evanch98@gmail.com>
2023-03-17 14:05:02 +0800

    Add Little Lemon Logo in OnBoarding Composable

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -1,2 +1,20 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OnBoarding() {
+    Image(
+        painter = painterResource(id = R.drawable.little_lemon_logo),
+        contentDescription = "Logo Image"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingPreview() {
+    OnBoarding()
+}
