c01e78674bb20a65e19a19a8775c42707a27cea6
Kyaw Thu <evanch98@gmail.com>
2023-03-27 17:59:17 +0800

    Change some UI design

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -1,15 +1,21 @@
 package com.example.littlelemon
 
+import android.graphics.Color
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Colors
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun OnBoarding() {
@@ -20,11 +26,18 @@ fun OnBoarding() {
         Image(
             painter = painterResource(id = R.drawable.little_lemon_logo),
             contentDescription = "Logo Image",
-            contentScale = ContentScale.Fit
         )
-        Text(
-            text = "Let's get to know you",
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .background(color = colorResource(id = R.color.green))
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = "Let's get to know you",
+            )
+        }
     }
 }
 
