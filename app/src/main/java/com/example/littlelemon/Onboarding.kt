91f9e1f003f357c12c1c0c95c68b9258605e444f
Kyaw Thu <evanch98@gmail.com>
2023-03-28 20:02:24 +0800

    Add TextField composables and button composable

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -3,13 +3,20 @@ package com.example.littlelemon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,6 +25,17 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun OnBoarding() {
 
+    var firstName by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+
+    var lastName by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+
+    var email by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -50,8 +68,60 @@ fun OnBoarding() {
                 .padding(20.dp),
             textAlign = TextAlign.Left
         )
-        Column {
-
+        Spacer(modifier = Modifier.height(30.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            OutlinedTextField(
+                value = firstName,
+                onValueChange = {
+                firstName = it
+            },
+                label = {Text(text = "First Name")},
+                placeholder = { Text(text = "Bob")},
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = colorResource(id = R.color.green),
+                    focusedLabelColor = colorResource(id = R.color.green)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = {
+                    lastName = it
+                },
+                label = {Text(text = "Last Name")},
+                placeholder = { Text(text = "Doe")},
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = colorResource(id = R.color.green),
+                    focusedLabelColor = colorResource(id = R.color.green)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedTextField(
+                value = email,
+                onValueChange = {
+                    email = it
+                },
+                label = {Text(text = "Email")},
+                placeholder = { Text(text = "bobdoe@email.com")},
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = colorResource(id = R.color.green),
+                    focusedLabelColor = colorResource(id = R.color.green)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            )
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Register")
         }
     }
 }
