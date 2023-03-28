ca203fb6b2f08afb868ba118a00c2e1f6ca2b2d8
Kyaw Thu <evanch98@gmail.com>
2023-03-29 00:14:43 +0800

    Add Destination interface, Home object, and Profile object for navigation

--- app/src/main/java/com/example/littlelemon/Destination.kt
+++ app/src/main/java/com/example/littlelemon/Destination.kt
@@ -1,2 +1,13 @@
 package com.example.littlelemon
 
+interface Destination {
+    val route: String
+}
+
+object Home: Destination {
+    override val route: String = "Home"
+}
+
+object Project: Destination {
+    override val route: String = "Profile"
+}
