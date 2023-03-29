bd3a235a3d751fb42d2ea5f8b9b7fbd321404073
Kyaw Thu <evanch98@gmail.com>
2023-03-29 23:25:01 +0800

    Dummy composable

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,2 +1,10 @@
 package com.example.littlelemon
 
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Home() {
+    Text(text = "Home", fontSize = 30.sp)
+}
--- app/src/main/java/com/example/littlelemon/Profile.kt
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -1,2 +1,10 @@
 package com.example.littlelemon
 
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Profile() {
+    Text(text = "Profile", fontSize = 30.sp)
+}
