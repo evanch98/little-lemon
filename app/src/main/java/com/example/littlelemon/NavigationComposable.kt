dfa31f09291eb83ab881352a8d36d1b9b763cfa2
Kyaw Thu <evanch98@gmail.com>
2023-03-29 19:14:31 +0800

    Add navigation composable

--- app/src/main/java/com/example/littlelemon/NavigationComposable.kt
+++ app/src/main/java/com/example/littlelemon/NavigationComposable.kt
@@ -1,2 +1,18 @@
 package com.example.littlelemon
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Home.route) {
+        composable(Home.route) {
+
+        }
+        composable(Profile.route) {
+
+        }
+    }
+}
