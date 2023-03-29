e7209159e9e0f98a1d3f8f21604af487a17b47d6
Kyaw Thu <evanch98@gmail.com>
2023-03-29 23:31:12 +0800

    StartDestination depends on whether the user is logged in or not

--- app/src/main/java/com/example/littlelemon/NavigationComposable.kt
+++ app/src/main/java/com/example/littlelemon/NavigationComposable.kt
@@ -1,21 +1,36 @@
 package com.example.littlelemon
 
+import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 @Composable
 fun Navigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = OnBoarding.route) {
+
+    val sharedPreferences =
+        LocalContext.current.getSharedPreferences("UserData", Context.MODE_PRIVATE)
+
+    var isLoggedIn = sharedPreferences.getBoolean("loggedIn", false)
+
+    NavHost(
+        navController = navController,
+        startDestination = if (isLoggedIn) {
+            Home.route
+        } else {
+            OnBoarding.route
+        }
+    ) {
         composable(OnBoarding.route) {
             OnBoarding(navController)
         }
         composable(Home.route) {
-
+            Home()
         }
         composable(Profile.route) {
-
+            Profile()
         }
     }
 }
