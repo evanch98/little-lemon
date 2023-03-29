63e4128517316f9b3c1ed8f6c8b0a31802cb0ca8
Kyaw Thu <evanch98@gmail.com>
2023-03-29 20:09:52 +0800

    Add OnBoarding object

--- app/src/main/java/com/example/littlelemon/Destination.kt
+++ app/src/main/java/com/example/littlelemon/Destination.kt
@@ -4,6 +4,10 @@ interface Destination {
     val route: String
 }
 
+object OnBoarding: Destination {
+    override val route: String = "OnBoarding"
+}
+
 object Home: Destination {
     override val route: String = "Home"
 }
