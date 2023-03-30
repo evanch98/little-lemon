225b0b89e5dabe2b29287a873e3cbb63837f215d
Kyaw Thu <evanch98@gmail.com>
2023-03-30 19:51:49 +0800

    Setting up the Profile Screen

--- app/src/main/java/com/example/littlelemon/Profile.kt
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -1,10 +1,110 @@
 package com.example.littlelemon
 
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Preview(showBackground = true)
 @Composable
 fun Profile() {
-    Text(text = "Profile", fontSize = 30.sp)
+
+    val sharedPreferences = LocalContext.current.getSharedPreferences(
+        "UserData", Context.MODE_PRIVATE
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.little_lemon_logo),
+            contentDescription = "Logo Image",
+            modifier = Modifier.width(200.dp),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.profile_pic),
+            contentDescription = "Profile Picture"
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                text = "Personal information",
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Left
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Full Name",
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Left
+            )
+            Text(
+                text = "${
+                    sharedPreferences.getString(
+                        "firstName",
+                        "user"
+                    )
+                } ${sharedPreferences.getString("lastName", "user")}",
+                fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Divider(modifier = Modifier.fillMaxWidth())
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "Email",
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Left
+            )
+            Text(
+                text = "${sharedPreferences.getString("email", "user@email.com")}",
+                fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Divider(modifier = Modifier.fillMaxWidth())
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {
+
+            },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = colorResource(id = R.color.yellow),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+        ) {
+            Text(text = "Log out", fontSize = 18.sp)
+        }
+    }
 }
