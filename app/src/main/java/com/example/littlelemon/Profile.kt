9e410e46ec405dfb06cb71a72a35a7856d121826
Kyaw Thu <evanch98@gmail.com>
2023-03-30 20:08:55 +0800

    Add log out feature

--- app/src/main/java/com/example/littlelemon/Profile.kt
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -18,10 +18,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
-@Preview(showBackground = true)
 @Composable
-fun Profile() {
+fun Profile(navController: NavController) {
 
     val sharedPreferences = LocalContext.current.getSharedPreferences(
         "UserData", Context.MODE_PRIVATE
@@ -95,7 +95,8 @@ fun Profile() {
         Spacer(modifier = Modifier.height(20.dp))
         Button(
             onClick = {
-
+                      sharedPreferences.edit().remove("UserData").apply()
+                navController.navigate(OnBoarding.route)
             },
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = colorResource(id = R.color.yellow),
