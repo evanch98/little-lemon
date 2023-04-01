b3b4f24096536606ddf8bd4dd9bf945c9bf28e2e
Kyaw Thu <evanch98@gmail.com>
2023-04-01 22:52:07 +0800

    Initial UI design of Hero section

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,14 +1,19 @@
 package com.example.littlelemon
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,6 +52,47 @@ fun Home(navController: NavController) {
                     .width(50.dp)
                     .clickable { navController.navigate(Profile.route) },
             )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = colorResource(id = R.color.green))
+                .padding(20.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Little Lemon",
+                    color = colorResource(id = R.color.yellow),
+                    fontSize = 40.sp,
+                    letterSpacing = 1.5.sp
+                )
+                Row {
+                    Column {
+                        Text(
+                            text = "Chicago",
+                            color = colorResource(id = R.color.light_grey),
+                            fontSize = 24.sp
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(
+                            text = "We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.",
+                            color = colorResource(id = R.color.light_grey),
+                            modifier = Modifier.width(200.dp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.hero_image),
+                        contentDescription = "Hero Image",
+                        modifier = Modifier.width(150.dp).clip(RoundedCornerShape(16.dp)),
+                        contentScale = ContentScale.Fit
+                    )
+                }
+            }
         }
     }
 }
